1410e82a8d9581152a4024a910ed4d3513632468
roman3tejas <155983891+roman3tejas@users.noreply.github.com>
2024-01-08 19:30:31 +0530

    Add files via upload

---
+++ Tejas Bale/Operators.py
@@ -0,0 +1,18 @@
+rt = int(input("Enter 1st number:"))
+tr = int(input("Enter 2nd number:"))
+
+print(rt, "+", tr, "=", rt+tr)
+print(rt, "-", tr, "=", rt-tr)
+print(rt, "*", tr, "=", rt*tr)
+print(rt, "/", tr, "=", rt/tr)
+print(rt, "%", tr, "=", rt%tr)
+print(rt, "**", tr, "=", rt**tr)
+print(rt, "//", tr, "=", rt//tr)
+
+
+print(rt, "==", tr, "is", (rt==tr))
+print(rt, "!=", tr, "is", (rt!=tr))
+print(rt, ">", tr, "is", (rt>tr))
+print(rt, "<", tr, "is", (rt<tr))
+print(rt, ">=", tr, "is", (rt>=tr))
+print(rt, "<=", tr, "is", (rt<=tr))
---
+++ Tejas Bale/datatype.py
@@ -0,0 +1,79 @@
+#str
+a = "Python"
+print(a)
+print(type(a))
+
+#str
+a1 = 'Computer'
+print(a1)
+print(type(a1))
+
+#int
+b = 28
+print(b)
+print(type(b))
+
+#float
+c = 28.2
+print(c)
+print(type(c))
+
+#complex
+d = 12+1j
+print(d)
+print(type(d))
+
+#list
+e = ["computer", "mouse", "cpu", 1, 12.12, "desktop"]
+print(e)
+print(type(e))
+
+#set
+f = {"pen", "book", "notebook", 2}
+print(f)
+print(type(f))
+
+#Tuple
+g = ("car", "Bike", 2, "truck", 2.28, False)
+print(g)
+print(type(g))
+
+#dict
+h = {"train" : "CSMT", "live" : 2, "view" : 12.2020, "camera" : True}
+print(h)
+print(type(h))
+
+#bool
+j = True
+j_1 = False
+print(j, j_1)
+print(type(j))
+print(type(j_1))
+
+#NoneType
+i = None
+print(i)
+print(type(i))
+
+k = "tejas" "bale" , "12"
+print(k)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
